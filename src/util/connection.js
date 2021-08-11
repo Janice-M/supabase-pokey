9d059635f6e36299119c3f510b29b78057cc9f73
Janice-M <janiceink001@gmail.com>
2021-08-11 18:01:10 +0300

    signup and sign in

--- src/util/connection.js
+++ src/util/connection.js
@@ -4,3 +4,14 @@ const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const REACT_APP_SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY);
+
+
+const signIn = async () => {
+    await supabase.auth.signIn({ email: credentials.email, password:credentials.password})
+    clear();
+}
+
+const signUp = async () => {
+    await supabase.auth.signUp({email : credentials.email, password: credentials.password});
+    clear ();
+}
