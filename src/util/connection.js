96d92dedaa64e52bb51b14023d6ab346ff4effac
Janice-M <janiceink001@gmail.com>
2021-08-11 17:53:51 +0300

    supabase client connnection

--- src/util/connection.js
+++ src/util/connection.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const REACT_APP_SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
 
-export const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_URL);
+export const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY);
